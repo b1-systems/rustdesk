8dd97af7038c66f56613fd120b16ad1387d35b97
Matthias Weiss <weiss@b1-systems.de>
2026-03-12 14:43:28 +0100

    New build option: stop before Debian packaging step

--- build.py
+++ build.py
@@ -141,6 +141,11 @@ def make_parser():
         action="store_true",
         help="Skip cargo build process, only flutter version + Linux supported currently",
     )
+    parser.add_argument(
+        "--debian-prepare-only",
+        action="store_true",
+        help="Stop Debian Build after generating temp folder and CONTROL file and package in a tar archive",
+    )
     if windows:
         parser.add_argument(
             "--skip-portable-pack",
@@ -328,7 +333,7 @@ def ffi_bindgen_function_refactor():
     )
 
 
-def build_flutter_deb(version, features):
+def build_flutter_deb(version, features, prepare_only):
     if not skip_cargo:
         system2(f"cargo build --features {features} --lib --release")
         ffi_bindgen_function_refactor()
@@ -367,13 +372,19 @@ def build_flutter_deb(version, features):
     generate_control_file(version)
     system2("cp -a ../res/DEBIAN/* tmpdeb/DEBIAN/")
     md5_file_folder("tmpdeb/")
-    system2("dpkg-deb -b tmpdeb rustdesk.deb;")
-
+    if prepare_only:
+        # do not complete debian packaging, leave job to OBS
+        system2("tar -czf rustdesk_raw.tar.gz tmpdeb/")
+        os.rename("rustdesk_raw.tar.gz", "../rustdesk_raw.tar.gz")
+    else:
+        # finalize debian package
+        system2("dpkg-deb -b tmpdeb rustdesk.deb;")
+        os.rename(
+            "rustdesk.deb",
+            "../rustdesk-connect_%s-1_%s.deb" % (version, get_deb_arch()),
+        )
     system2("/bin/rm -rf tmpdeb/")
     system2("/bin/rm -rf ../res/DEBIAN/control")
-    os.rename(
-        "rustdesk.deb", "../rustdesk-connect_%s-1_%s.deb" % (version, get_deb_arch())
-    )
     os.chdir("..")
 
 
@@ -580,7 +591,7 @@ def main():
             else:
                 # system2(
                 #     'mv target/release/bundle/deb/rustdesk*.deb ./flutter/rustdesk.deb')
-                build_flutter_deb(version, features)
+                build_flutter_deb(version, features, args.debian_prepare_only)
         else:
             system2("cargo bundle --release --features " + features)
             if osx:
