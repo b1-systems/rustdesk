d079f4e06a528f0e63fd843ac14d3495e98ed523
Matthias Weiss <weiss@b1-systems.de>
2026-08-05 11:16:22 +0200

    Publish tar.gz archive

--- build.py
+++ build.py
@@ -321,7 +321,7 @@ def ffi_bindgen_function_refactor():
         'sed -i "s/ffi.NativeFunction<ffi.Bool Function(DartPort/ffi.NativeFunction<ffi.Uint8 Function(DartPort/g" flutter/lib/generated_bridge.dart')
 
 
-def build_flutter_deb(version, features):
+def build_flutter_deb(version, features, prepare_only):
     if not skip_cargo:
         system2(f'cargo build --locked --features {features} --lib --release')
         ffi_bindgen_function_refactor()
@@ -362,7 +362,17 @@ def build_flutter_deb(version, features):
     generate_control_file(version)
     system2('cp -a ../res/DEBIAN/* tmpdeb/DEBIAN/')
     md5_file_folder("tmpdeb/")
-    system2('dpkg-deb -b tmpdeb rustdesk.deb;')
+    if prepare_only:
+        # do not complete debian packaging, leave job to OBS
+        system2("tar -czf rustdesk_raw.tar.gz tmpdeb/")
+        os.rename("rustdesk_raw.tar.gz", "../rustdesk_raw.tar.gz")
+    else:
+        # finalize debian package
+        system2("dpkg-deb -b tmpdeb rustdesk.deb;")
+        os.rename(
+            "rustdesk.deb",
+            "../rustdesk-connect_%s-1_%s.deb" % (version, get_deb_arch()),
+        )
 
     system2('/bin/rm -rf tmpdeb/')
     system2('/bin/rm -rf ../res/DEBIAN/control')
@@ -567,7 +577,7 @@ def main():
             else:
                 # system2(
                 #     'mv target/release/bundle/deb/rustdesk*.deb ./flutter/rustdesk.deb')
-                build_flutter_deb(version, features)
+                build_flutter_deb(version, features, args.debian_prepare_only)
         else:
             system2('cargo --locked bundle --release --features ' + features)
             if osx:
